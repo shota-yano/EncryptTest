1853ce48a192ad67a83a701c0e6363e8157b5a34
shota-yano <44024424+shota-yano@users.noreply.github.com>
2023-01-01 06:48:12 +0900

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EncryptTest
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSelectFileForEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("clicked", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+
+        private void SCR_Main_Load(object sender, RoutedEventArgs e)
+        {
+            lblFilepath.Content = @"C:\work\EncryptWork\フルーツ.xlsx";
+        }
+
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            StreamEncrypter objEncrypter = new StreamEncrypter();
+            string strAfterFileName = $@"\暗号化後_{System.IO.Path.GetFileName(@lblFilepath.Content.ToString())}";
+            string strAfterFilePath = System.IO.Path.GetDirectoryName(@lblFilepath.Content.ToString())+strAfterFileName;
+
+            using (var inStream = File.Open(@lblFilepath.Content.ToString(), FileMode.Open))
+            using (var outStream = File.Open(@strAfterFilePath, FileMode.Create,FileAccess.Write))
+            {
+                objEncrypter.Encrypt(inStream, outStream);
+                Console.WriteLine($"Key:{Convert.ToBase64String(objEncrypter.Key)}");
+                Console.WriteLine($"IV:{Convert.ToBase64String(objEncrypter.IV)}");
+
+            }
+
+            lblFilePathforDecrypt.Content = @strAfterFilePath;
+            lblKey.Content = Convert.ToBase64String(objEncrypter.Key);
+            lblIV.Content = Convert.ToBase64String(objEncrypter.IV);
+            MessageBox.Show("暗号完了", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+            using (FileStream fs = new FileStream(strAfterFilePath, FileMode.Open, FileAccess.Write))
+            {
+                //初期ベクターをファイルの先頭に出力
+                fs.Write(objEncrypter.IV, 0, objEncrypter.IV.Length);
+            }
+                
+
+
+
+
+        }
+
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            var key = lblKey.Content.ToString(); //"3k93BexTujSrdlwQB0DyNx8cSuIxA22douhCa+eJqFE=";
+            var iv = lblIV.Content.ToString(); // "rmcWf+Wfv/B8MPUjjLsv2Q==";
+            
+
+            var objEncrypter = new StreamEncrypter(Convert.FromBase64String(key));
+            using (var inStream = File.Open(@lblFilePathforDecrypt.Content.ToString(), FileMode.Open))
+            using (var outStream = File.Open(@"C:\work\EncryptWork\復号化後_フルーツ.xlsx", FileMode.Create))
+            {
+                // ファイルの最初の16バイトから初期化ベクターを読み込みます。
+                byte[] temp_iv = new byte[16];
+                inStream.Read(temp_iv, 0, temp_iv.Length);
+                objEncrypter.Decrypt(inStream, outStream, temp_iv);
+            }
+
+            MessageBox.Show("復号完了", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ StreamEncrypter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace EncryptTest
+{
+    class StreamEncrypter
+    {
+        // コンストラクター
+        public StreamEncrypter(byte[] key = null, byte[] iv = null)
+        {
+            using (var aes = new AesManaged())
+            {
+                Key = key;
+                if (key == null)
+                {
+                    aes.GenerateKey();
+                    Key = aes.Key;
+                }
+                IV = iv;
+                if (iv == null)
+                {
+                    aes.GenerateIV();
+                    IV = aes.IV;
+                }
+            }
+        }
+        public byte[] Key { get; private set; }
+        public byte[] IV { get; private set; }
+
+        // 暗号化する
+        public void Encrypt(Stream inStream, Stream outStream)
+        {
+            using (var rmCrypto = new AesManaged())
+            {
+                rmCrypto.BlockSize = 128;
+                rmCrypto.KeySize = 128;
+
+                var encryptor = rmCrypto.CreateEncryptor(Key, IV);
+                using (var cryptoStream = new CryptoStream(outStream, encryptor, CryptoStreamMode.Write))
+                {
+                    // 暗号化されてcryptoStreamにデータが書き込まれる
+                    inStream.CopyTo(cryptoStream);
+
+
+                }
+            }
+        }
+        // 復号する
+        public void Decrypt(Stream inStream, Stream outStream , byte[] inIV)
+        {
+            using (var rmCrypto = new AesManaged())
+            {
+                rmCrypto.BlockSize = 128;
+                rmCrypto.KeySize = 128;
+
+                //var decryptor = rmCrypto.CreateDecryptor(Key, IV);
+                var decryptor = rmCrypto.CreateDecryptor(Key, inIV);
+                using (var cryptoStream = new CryptoStream(inStream, decryptor, CryptoStreamMode.Read))
+                {
+                    // 復号されてoutStreamにデータが書き込まれる
+                    //cryptoStream.CopyTo(outStream);
+                    byte[] buffer = new byte[8192];
+                    int len = 0;
+                    while ((len = cryptoStream.Read(buffer, 0, 8192)) > 0)
+                    {
+                        outStream.Write(buffer, 0, len);
+                    }
+                }
+            }
+        }
+
+    }
+}
